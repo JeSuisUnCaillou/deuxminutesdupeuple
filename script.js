14a23313e12c26294acd60b628ee8da179c37208
jesuisuncaillou <valentin.shamsnejad@soprabanking.com>
2019-07-02 14:59:34 +0200

    get names too

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+var http = require('https')
 const root = 'https://les2minutesdupeuple.tk'
 
 const readFile = async (fileName) => {
@@ -10,14 +11,42 @@ const readFile = async (fileName) => {
 }
 
 const extractUrls = (data) => {
-  const matchs = data.match(/href="(.+?)"/g)
-  return matchs.map(match => match.substring(6, match.length - 1))
+  const matchs = data.match(/href="(.+?)".+?<\/span>/g)
+  return matchs.map(match => {
+    const file = match.match(/href="(.+?)"/)[1]
+    var title = match.match(/span>(.+?)<\/span/)[1]
+    title = title.replace('Ã©', 'é')
+    title = title.replace('Ã´', 'ô')
+    title = title.replace('Ã¨', 'è')
+    title = title.replace('Ã ', 'à')
+    title = title.replace('Ãª', 'ê')
+    title = title.replace('Ã«', 'ë')
+    title = title.replace('\\', '')
+    title = title.replace('Ã', 'Ç')
+    
+    return { file, title }
+  })
+}
+
+const downloadFile = (url, fileName, callback) => {
+  var file = fs.createWriteStream(fileName)
+  http.get(url, (response) => {
+    response.pipe(file)
+    file.on('finish', () => {
+      file.close(callback)
+    })
+  })
 }
 
 const main = async () => {
   const fileContent = await readFile('./data_origin.xml')
   const results = extractUrls(fileContent)
+
+  const file = results[0]
   console.log(results)
+  // downloadFile(root + file, 'test.mp3', (data, error) => {
+  //   if (error) { console.log(error) } else { console.log(data) }
+  // })
 }
 
 main()
